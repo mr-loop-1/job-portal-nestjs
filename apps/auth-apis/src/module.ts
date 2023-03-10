39be3514ba0d5e86646e0a6f2d8bb7cb1a6f06d9
Abdul Samad <abdulsamad@squareboats-MacBook-Air.local>
2023-03-10 16:44:54 +0530

    some config only

--- apps/auth-apis/src/module.ts
+++ apps/auth-apis/src/module.ts
@@ -6,6 +6,8 @@ import { AuthService } from './services/service';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AdminController } from './controllers/admin';
+import mail from '@config/mail';
+import { MailmanModule } from '@squareboat/nest-mailman';
 
 @Module({
   imports: [
@@ -15,6 +17,18 @@ import { AdminController } from './controllers/admin';
       imports:[ConfigModule],
       useFactory:async (config:ConfigService)=>config.get('auth') ,
       inject:[ConfigService]
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      expandVariables: true,
+      load: [mail],
+    }),
+    MailmanModule.registerAsync({
+      imports: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        return config.get("mailman");
+      },
+      inject: [ConfigService],
     })
   ],
   controllers: [UserController, AdminController],
---
+++ config/mail.ts
@@ -0,0 +1,42 @@
+import { registerAs } from "@nestjs/config";
+import { MailmanOptions } from "@squareboat/nest-mailman";
+import { GenericMail } from "resources/views/mail";
+
+export default registerAs(
+  "mailman",
+  () =>
+    ({
+      host: process.env.MAIL_HOST,
+      port: +process.env.MAIL_PORT,
+      username: process.env.MAIL_USERNAME,
+      password: process.env.MAIL_PASSWORD,
+      from: process.env.MAIL_SENDER_ID,
+      templateConfig: {
+        baseComponent: GenericMail,
+        templateOptions: {
+          socialMedia: [
+            {
+              name: "youtube-noshare",
+              href: "https://www.youtube.com/channel/UCyUzPvikgKBYTfp4XBIbbOg",
+            },
+            {
+              name: "instagram-noshare",
+              href: "https://www.instagram.com/square.boat/",
+            },
+            {
+              name: "linkedin-noshare",
+              href: "https://www.linkedin.com/company/squareboat",
+            },
+            {
+              name: "github-noshare",
+              href: "https://github.com/squareboat",
+            },
+          ],
+          appLogoSrc:
+            "https://i.ibb.co/7VrFQwN/squareboat-logo-transparent.png",
+          appName: "abd sam",
+          contactEmail: "abdul.samad@squareboat.com",
+        },
+      },
+    } as MailmanOptions)
+);
