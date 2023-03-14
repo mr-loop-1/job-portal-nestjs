a48e38b12ff477bdfee225599e66c4f2388f8be1
Abdul Samad <abdulsamad@squareboats-MacBook-Air.local>
2023-03-14 15:37:06 +0530

    REFACTOR: separated body and req

--- apps/auth-apis/src/controllers/admin.ts
+++ apps/auth-apis/src/controllers/admin.ts
@@ -1,5 +1,13 @@
 import { Dto, Validate } from '@libs/boat/validator';
-import { Body, Controller, Get, Post, Req, Res, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Req,
+  Res,
+  UseGuards,
+} from '@nestjs/common';
 import { AdminLoginDto } from '../dto/adminLogin';
 import { AuthService } from '../services/service';
 import { RestController, Request, Response } from '@libs/boat';
@@ -7,18 +15,23 @@ import { UserTransformer } from '../transformers/user';
 
 @Controller('admin')
 export class AdminController extends RestController {
+  constructor(private readonly authService: AuthService) {
+    super();
+  }
 
-    constructor(private readonly authService: AuthService) { 
-        super();
-    };
-
-    @Validate(AdminLoginDto)
-    @Post('login')
-    async adminLogin(@Req() inputs: Request, @Res() res: Response): Promise<Response> {
-
-        const result = await this.authService.adminLogin(inputs.body.email, inputs.body.password);
-        return res.success(await this.transform(result, new UserTransformer, { inputs }))
-
-    }
-
+  @Validate(AdminLoginDto)
+  @Post('login')
+  async adminLogin(
+    @Dto() inputs: AdminLoginDto,
+    @Req() req: Request,
+    @Res() res: Response,
+  ): Promise<Response> {
+    const result = await this.authService.adminLogin(
+      inputs.email,
+      inputs.password,
+    );
+    return res.success(
+      await this.transform(result, new UserTransformer(), { req }),
+    );
+  }
 }
--- apps/auth-apis/src/controllers/user.ts
+++ apps/auth-apis/src/controllers/user.ts
@@ -1,4 +1,4 @@
-import { Validate } from '@libs/boat/validator';
+import { Dto, Validate } from '@libs/boat/validator';
 import { Controller, Post, Req, Res } from '@nestjs/common';
 import { AuthService } from '../services/service';
 import { RestController, Request, Response } from '@libs/boat';
@@ -10,44 +10,60 @@ import { UserTransformer } from '../transformers/user';
 
 @Controller('user')
 export class UserController extends RestController {
+  constructor(private readonly authService: AuthService) {
+    super();
+  }
 
-    constructor(private readonly authService: AuthService) {
-        super();
-    };
+  @Validate(UserRegisterDto)
+  @Post('register')
+  async registerUser(
+    @Dto() inputs: UserRegisterDto,
+    @Req() req: Request,
+    @Res() res: Response,
+  ): Promise<Response> {
+    const result = await this.authService.addUser(inputs);
+    return res.success(
+      await this.transform(result, new UserTransformer(), { req }),
+    );
+  }
 
-    @Validate(UserRegisterDto)
-    @Post('register')
-    async registerUser(@Req() inputs: Request, @Res() res: Response) : Promise<Response> {
-        
-        const result = await this.authService.addUser(inputs.body);
-        return res.success(await this.transform(result, new UserTransformer, { inputs }));
+  @Validate(UserLoginDto)
+  @Post('/login')
+  async loginUser(
+    @Dto() inputs: UserLoginDto,
+    @Req() req: Request,
+    @Res() res: Response,
+  ): Promise<Response> {
+    const result = await this.authService.userLogin(
+      inputs.email,
+      inputs.password,
+    );
+    return res.success(
+      await this.transform(result, new UserTransformer(), { req }),
+    );
+  }
 
-    }
+  @Validate(forgotPasswordDto)
+  @Post('forgot-password')
+  async forgotPassword(
+    @Dto() inputs: forgotPasswordDto,
+    @Req() req: Request,
+    @Res() res: Response,
+  ): Promise<Response> {
+    const result = await this.authService.forgotPasswordHandler(inputs.email);
+    return res.success(result);
+  }
 
-    @Validate(UserLoginDto)
-    @Post('/login')
-    async loginUser(@Req() inputs: Request, @Res() res: Response) : Promise<Response> {
-        
-        const result = await this.authService.userLogin(inputs.body.email, inputs.body.password);
-        return res.success(await this.transform(result, new UserTransformer, { inputs }));
-
-    }
-
-    @Validate(forgotPasswordDto)
-    @Post('forgot-password')
-    async forgotPassword(@Req() input: Request, @Res() res: Response) : Promise<Response> {
-        
-        const result = await this.authService.forgotPasswordHandler(input.body.email);
-        return res.success(result);
-
-    }
-
-    @Validate(resetPasswordDto)
-    @Post('reset-password')
-    async resetPassword(@Req() inputs: Request, @Res() res: Response) : Promise<Response> {
-
-        const result = await this.authService.resetUser(inputs.body);
-        return res.success(await this.transform(result, new UserTransformer, { inputs }));;
-
-    }
+  @Validate(resetPasswordDto)
+  @Post('reset-password')
+  async resetPassword(
+    @Dto() inputs: resetPasswordDto,
+    @Req() req: Request,
+    @Res() res: Response,
+  ): Promise<Response> {
+    const result = await this.authService.resetUser(inputs);
+    return res.success(
+      await this.transform(result, new UserTransformer(), { req }),
+    );
+  }
 }
