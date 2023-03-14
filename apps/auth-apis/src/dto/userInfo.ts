2446d60a75340ce3876e3e8f841c0d6a9dc180a3
Abdul Samad <abdulsamad@squareboats-MacBook-Air.local>
2023-03-14 15:27:55 +0530

    REFACTOR: moved if checks to beginning and negated them

--- apps/auth-apis/src/dto/userInfo.ts
+++
@@ -1,8 +0,0 @@
-export class UserInfo {
-    name: string
-    email: string
-    role: number
-    skills: string
-    mobileNo: string
-    token: string
-}
--- apps/auth-apis/src/services/service.ts
+++ apps/auth-apis/src/services/service.ts
@@ -8,114 +8,102 @@ import { ulid } from 'ulid';
 import { resetPasswordDto } from '../dto/resetPassword';
 import { UserRegisterDto } from '../dto/userRegister';
 
-
 @Injectable()
 export class AuthService {
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly userService: UserLibService,
+  ) {}
 
-    constructor(
-        private readonly jwtService: JwtService,
-        private readonly userService: UserLibService
-    ) { };
+  async addUser(inputs: UserRegisterDto): Promise<IUser> {
+    const createUser = {
+      ulid: ulid(),
+      name: inputs.name,
+      email: inputs.email,
+      password: inputs.password,
+      skills: inputs.skills,
+      mobileNo: inputs.mobileNo,
+      role: inputs.role,
+    };
 
-    async addUser(inputs: UserRegisterDto): Promise<IUser> {
+    const newUser = await this.userService.repo.create({
+      ...createUser,
+    });
 
-        const createUser = {
-            ulid: ulid(),
-            name: inputs.name,
-            email: inputs.email,
-            password: inputs.password,
-            skills:  inputs.skills,
-            mobileNo: inputs.mobileNo,
-            role: inputs.role
-        }
+    const token = await this.generateToken(newUser);
+    return { ...newUser, token };
+  }
 
-        const newUser = await this.userService.repo.create({
-            ...createUser
-        })
+  async adminLogin(email: string, password: string): Promise<IUser> {
+    const admin = await this.userService.repo.firstWhere({
+      email: email,
+      password: password,
+    });
+    const token = await this.generateToken(admin);
+    return { ...admin, token: token };
+  }
 
-        const token = await this.generateToken(newUser);
-        return {...newUser, token};
+  async userLogin(email: string, password: string): Promise<IUser> {
+    const user = await this.userService.repo.firstWhere({
+      email: email,
+      password: password,
+    });
+    const token = await this.generateToken(user);
+    return { ...user, token: token };
+  }
+
+  async generateToken(user: IUser): Promise<string> {
+    const payload = {
+      sub: user.id,
+      name: user.name,
+      role: user.role,
+    };
+    return await this.jwtService.signAsync(payload);
+  }
+
+  async forgotPasswordHandler(email: string): Promise<string> {
+    if (await this.userService.repo.existsUserEmail(email)) {
+      throw new HttpException("Email doesn't exists", HttpStatus.FORBIDDEN);
     }
 
-    async adminLogin(email: string, password: string): Promise<IUser> {
+    const key = CacheKeys.build(CacheKeys.FORGOT_PASSWORD, {
+      email: email,
+    });
 
-        const admin = await this.userService.repo.firstWhere({ email: email, password: password });
-        const token = await this.generateToken(admin);
-        return {...admin, token: token};
+    const otp = Math.floor(Math.random() * 10000);
+    console.log('otp = ', `${otp}`);
 
+    await CacheStore().set(key, `${otp}`, 10 * 60);
+
+    return 'Otp sent on mail';
+  }
+
+  async resetUser(inputs: resetPasswordDto): Promise<any> {
+    if (await this.userService.repo.existsUserEmail(inputs.email)) {
+      throw new HttpException("Email doesn't exists", HttpStatus.FORBIDDEN);
     }
 
-    async userLogin(email: string, password: string): Promise<IUser> {
-        
-        const user = await this.userService.repo.firstWhere({ email: email, password: password });
-        const token = await this.generateToken(user);
-        return {...user, token: token};
+    const key = CacheKeys.build(CacheKeys.FORGOT_PASSWORD, {
+      email: inputs.email,
+    });
 
+    if (!(await CacheStore().has(key))) {
     }
 
-    async generateToken(user: IUser) : Promise<string> {
-        const payload = { 
-            sub: user.id, 
-            name: user.name, 
-            role: user.role
-        };
-        return await this.jwtService.signAsync(payload);
+    if ((await CacheStore().get(key)) !== inputs.otp) {
+      throw new HttpException('Incorrect Otp entered', HttpStatus.UNAUTHORIZED);
     }
 
-    async forgotPasswordHandler(email: string) : Promise<string> {
+    const updatedUser = await this.userService.repo.updateWhere(
+      { email: inputs.email },
+      { password: inputs.newPassword },
+    );
 
-        if(await this.userService.repo.existsUserEmail(email)) {
-
-            const key = CacheKeys.build(
-                CacheKeys.FORGOT_PASSWORD, {
-                    email: email
-                }
-            );
-
-            console.log("kwy = ",key);
-
-            const otp = await CacheStore().get(key);
-            console.log('otp = ',`${otp}`);
-            
-            await CacheStore().set(key, `${otp}`, 10*60);
-
-            return "Otp sent on mail"
-        }
-        else {
-            throw new HttpException("Email doesn't exists", HttpStatus.FORBIDDEN);
-        }
-    }
-
-    async resetUser(inputs: resetPasswordDto) : Promise<any> {
-
-        if(await this.userService.repo.existsUserEmail(inputs.email)) {
-
-            const key = CacheKeys.build(
-                CacheKeys.FORGOT_PASSWORD, {
-                    email: inputs.email
-                }
-            );
-    
-            const storedOtp = await CacheStore().get(key);
-    
-            if(`${storedOtp}` === inputs.otp) {
-    
-                const updatedUser = await this.userService.repo.updateWhere(
-                    {email: inputs.email},
-                    {password: inputs.newPassword}
-                )
-                
-                const user = await this.userService.repo.firstWhere({ email: inputs.email, password: inputs.newPassword });
-                const token = await this.generateToken(user);
-                return {...user, token};
-            }
-            else {
-                throw new HttpException('Incorrect Otp entered', HttpStatus.UNAUTHORIZED)
-            }
-        }
-        else {
-            throw new HttpException("Email doesn't exists", HttpStatus.FORBIDDEN);
-        }
-
-    }
+    const user = await this.userService.repo.firstWhere({
+      email: inputs.email,
+      password: inputs.newPassword,
+    });
+    const token = await this.generateToken(user);
+    return { ...user, token };
+  }
 }
