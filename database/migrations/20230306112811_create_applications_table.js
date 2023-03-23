7dae498639d305de3bfcad2afb97e0a2f517dcb5
Abdul Samad <abdulsamad@squareboats-MacBook-Air.local>
2023-03-23 13:17:18 +0530

    FIX: unknownn

--- database/migrations/20230306112811_create_applications_table.js
+++ database/migrations/20230306112811_create_applications_table.js
@@ -1,26 +1,26 @@
-const { timestamps, onUpdateTrigger } = require("../utils");
+const { timestamps, onUpdateTrigger } = require('../utils');
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = async knex => {
-    const migration = await knex.schema.createTable('applications', (table) => {
-        table.bigIncrements('id').index();
-        table.string('ulid');
-        table.bigInteger('candidateId').index().notNullable();
-        table.bigInteger('jobId').index().notNullable();
-        table.integer('status').index();
-        timestamps(knex, table);
-    })
-    await knex.raw(onUpdateTrigger('applications'));
-    return migration;
+exports.up = async (knex) => {
+  const migration = await knex.schema.createTable('applications', (table) => {
+    table.bigIncrements('id').index();
+    table.string('ulid');
+    table.bigInteger('candidateId').index().notNullable();
+    table.bigInteger('jobId').index().notNullable();
+    table.integer('status').index();
+    timestamps(knex, table);
+  });
+  await knex.raw(onUpdateTrigger('applications'));
+  return migration;
 };
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = async knex => {
-    await knex.schema.dropTableIfExists('applications');
+exports.down = async (knex) => {
+  await knex.schema.dropTableIfExists('applications');
 };
